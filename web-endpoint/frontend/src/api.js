b46a69720f909c29ed4528999a571f55d583e4e7
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2026-02-08 14:08:56 +0500

    Changed http->https

--- web-endpoint/frontend/src/api.js
+++ web-endpoint/frontend/src/api.js
@@ -1,5 +1,5 @@
 export async function generateText(prompt) {
-  const res = await fetch("http://llm.strangebit.io:/generate", {
+  const res = await fetch("https://llm.strangebit.io:/generate", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ text: prompt })
